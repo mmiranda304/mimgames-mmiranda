e343c3d7e70502dbe82ace1e9992114e423bf5af
Manuel Miranda <manuel.miranda304@gmail.com>
2022-04-22 10:39:23 -0300

    Adding  ItemCount (Desafio 4)

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './ItemCount.css'
 
-function ItemCount() {
+function ItemCount({stock, initial, onAdd }) {
+  const [cuenta, setCuenta] = useState(1);
+  
+  const handleAdd = () => {
+    ( (cuenta+1) <= stock ) ? setCuenta(cuenta+1) : setCuenta(cuenta);
+  }
+  const handleTake = () => {
+    ( (cuenta-1) >= (initial) ) ? setCuenta(cuenta-1) : setCuenta(cuenta);
+  }
+
   return (
-    <div>ItemCount</div>
+    <div className='item-count'>
+      <div className='keypad-cart'>
+        <button className='btn-q' onClick={() => handleTake() } >-</button>
+        <p className='Quantity'> {cuenta} </p>
+        <button className='btn-q' onClick={() => handleAdd() } >+</button>
+      </div>
+        <button className='btn-cart' onClick={ () => {(cuenta<=stock)?onAdd():console.log("No hay stock")} } >Agregar al carrito</button>
+    </div>
   )
 }
-
 export default ItemCount
-
-// OnAdd se va a realizar en itemListContainer
-//Si queremos usar numeros en vez de strings es mejor
-// por ejemplo stock={5}
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './ItemListContainer.css'
+import BG_BHH from '../img/BUNKIN_1.jpg'
+import ItemCount from '../ItemCount/ItemCount'
 
 function ItemListContainer({greeting}) {  
+
+  const onAdd = () => {     {/* En cumplimiento con desafío N° 4 */}
+    return console.log("Agregado al carrito");
+  }
+
   return (
-    <h1>{greeting}</h1>
+    <>
+      <h1>{greeting}</h1>   {/*En cumplimiento con desafío N° 3*/}
+
+      <div className='item'>
+        <div className='img-container'>
+          <img src={BG_BHH} alt="logo"/>
+        </div>
+        <div className='item-body'>
+          <h5 className='item-title'>Bunny Kingdom</h5>
+          <p className='item-text'>$ 13.900</p>
+          <ItemCount stock={5} initial={1} onAdd={onAdd} />
+        </div>
+      </div>
+    </>
   )
 }
 export default ItemListContainer
